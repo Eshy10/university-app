7c59c83be798a3b5607e1ec9bc61600f8411ca87
modupe <adewalebose15@gmail.com>
2020-06-03 19:49:58 +0100

    Update student Profile

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,6 @@
 class StudentsController < ApplicationController
+before_action :find_student, only: [:show, :edit, :update]
+
   def index
     @students = Student.all
   end
@@ -8,15 +10,30 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def create
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = 'Your profile was successfully created'
-      redirect_to root_path
+      redirect_to @student
     else
       render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = 'Your Profile was succesfully created'
+      redirect_to @student
+    else
+      render :edit
     end
   end
 
@@ -24,5 +41,9 @@ class StudentsController < ApplicationController
 
   def student_params
     params.require(:student).permit(:name, :email)
+  end
+
+  def find_student
+    @student = Student.find(params[:id])
   end
 end
